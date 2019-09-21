aa0bcfe680e001dae9844caf2146fb0b6df13dfc
Kane <spearskane@gmail.com>
2019-09-21 18:32:59 -0400

    p3 complete

--- spears_PA2/spears_p3/src/spears_p3/PollProgram.java
+++ spears_PA2/spears_p3/src/spears_p3/PollProgram.java
@@ -1,5 +1,5 @@
 package spears_p3;
-import java.util.*;
+import java.util.Scanner;
 
 /*
  * Write a simple polling program that allows users to 
@@ -19,5 +19,83 @@ import java.util.*;
  */
 
 public class PollProgram {
-
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		String[] topics = {"Food", "Money", "Soccer", "Books", "Friends"};
+		int[][] ratings = new int[topics.length][];
+		int people, rating, i, j;
+		
+		for (i = 0; i < ratings.length; i++) {
+			ratings[i] = new int[10];
+			for (j = 0; j < ratings[i].length; j++) {
+				ratings[i][j] = 0;
+			}
+		}
+		
+		System.out.print("Enter the number of respondents: ");
+		people = scnr.nextInt();
+		
+		for (i = 0; i < people; i++) {
+			System.out.println("Rate each topic on a scale of 1 - 10 for the " + topics.length + " topics below: ");
+			for (j = 0; j < topics.length; j++) {
+				System.out.print("Enter your rating for " + topics[j] + ": ");
+				rating = scnr.nextInt();
+				while (rating < 1 || rating > 10) {
+					System.out.println("Rating should be from 1 - 10");
+					System.out.print("Enter your raiting for " + topics[j] + ": ");
+					rating = scnr.nextInt();
+				}
+				ratings[j][rating - 1]++;	
+			}
+		}
+		
+		int avgRating[] = new int[topics.length];
+		String highestPointTopic = "", lowestPointTopic = "";
+		int highestPointTotal = 0, lowestPointTotal = 0;
+		
+		for (i = 0; i < ratings.length; i++) {
+			avgRating[i] = 0;
+			for (j = 0; j < ratings[i].length; j++) {
+				avgRating[i] += (ratings[i][j] * (j + 1));
+			}
+			if (i == 0) {
+				highestPointTotal = avgRating[i];
+				lowestPointTotal = avgRating[i];
+				highestPointTopic = topics[i];
+				lowestPointTopic = topics[i];
+			}
+			else {
+				if (avgRating[i] > highestPointTotal) {
+					highestPointTopic = topics[i];
+					highestPointTotal = avgRating[i];
+				}
+				if (avgRating[i] < lowestPointTotal) {
+					lowestPointTopic = topics[i];
+					lowestPointTotal = avgRating[i];
+				}
+			}
+			avgRating[i] = avgRating[i] / people;
+		}
+		
+		System.out.printf("\n%-20s","");
+		for (i = 0; i < 10; i++) {
+			System.out.printf("%-10d", (i + 1));
+		}
+		
+		System.out.printf("%20s","Average Rating");
+		System.out.println();
+		
+		for (i = 0; i < ratings.length; i++) {
+			System.out.printf("\n%-20s", topics[i]);
+			for (j = 0; j < ratings[i].length; j++) {
+				System.out.printf("%-10d", ratings[i][j]);
+			}
+			System.out.printf("%20d", avgRating[i]);
+		}
+		
+		System.out.println("\n\nHighest total points: " + highestPointTotal + "\nTopic: " + highestPointTopic);
+		System.out.println("\nLowest total points: " + lowestPointTotal + "\nTopic: " + lowestPointTopic);
+		
+		scnr.close();
+	}
 }
